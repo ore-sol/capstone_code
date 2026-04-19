cda565ec6076057ba5b1a47161fb2cee7c88c361
ore-sol <solanke.ore@gmail.com>
2026-04-19 14:35:53 +0100

    working on files for jiayi code

---
+++ code/pseudo.R
@@ -0,0 +1,7 @@
+## take in csv files from output 
+
+## grab output files from analysis software 
+
+## rank soils by microorg composition (relative)
+
+## feed ranking data into random forest machine learning model 
